f76789aa2d546179957a2edf26302458715b5459
Abishek Aranganathan <abisheka@avecsage.com>
2024-06-27 18:06:21 +0530

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
     host: 'https://localhost:4200',
-    hosturl:'https://clubmanage.azurewebsites.net',
+    // hosturl:'https://clubmanage.azurewebsites.net',
+    hosturl:'https://netclubapi.azurewebsites.net',
     //  myurl:'https://clubmanageapi.azurewebsites.net',
-    myurl: 'https://webappbuild.azurewebsites.net'
+    myurl: 'https://netclubapi.azurewebsites.net'
     // myurl:'https://clubmanageapi.azurewebsites.net',
 //    myurl: 'https://localhost:7251',
 
